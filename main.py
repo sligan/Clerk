d841c62601ce88cdf5c8284c19740435628a6f4b
AlexandrSligan <sligan.alex@gmail.com>
2021-07-21 21:28:28 +0700

    add rollbar

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ from flask import Flask
 from dotenv import load_dotenv
 import slack
 import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 
 load_dotenv()
 app = Flask(__name__)
 client = slack.WebClient(token=os.getenv('TOKEN'))
 slack_event_adapter = SlackEventAdapter(os.getenv('SECRET'), '/slack/events', app)
+
+
+@app.before_first_request
+def init_rollbar():
+    rollbar.init(os.getenv('ACCESS_TOKEN'), environment='development')
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 
 if __name__ == '__main__':
