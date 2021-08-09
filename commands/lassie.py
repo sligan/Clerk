876ee7a8df24eb56455ade3c784d664ba3805bad
AlexandrSligan <sligan.alex@gmail.com>
2021-08-09 14:13:16 +0700

    fix lassie commands

--- commands/lassie.py
+++ commands/lassie.py
@@ -1,6 +1,6 @@
 import main
 from flask import request, Response
-from db_connect import lessie_get
+from db_connect import lassie_get
 from datetime import datetime
 from main import app, client
 from commands import timestamp
@@ -8,7 +8,7 @@ from commands import timestamp
 
 @app.route('/lassie-users-total', methods=['POST'])
 def lessie_users_total():
-    users = lessie_get("""SELECT count(id) FROM users""")
+    users = lassie_get("""SELECT count(id) FROM users""")
     data = request.form
     channel_id = data.get('channel_id')
     client.chat_postMessage(channel=channel_id, text=f'Lessie Smoke users total - {users.iloc[0]["count"]}')
@@ -19,36 +19,35 @@ def lessie_users_total():
 def lassie_day():
     data = request.form
     channel_id = data.get('channel_id')
-    count_new_users_id = lessie_get(query="SELECT count(distinct(id)) FROM users WHERE created_at > now() - interval "
+    count_new_users_id = lassie_get(query="SELECT count(distinct(id)) FROM users WHERE created_at > now() - interval "
                                           "'24 hours'")
-    count_active_users = lessie_get(query="SELECT count(distinct(user_id)) FROM smoking_actions WHERE smoke_at > "
+    count_active_users = lassie_get(query="SELECT count(distinct(user_id)) FROM smoking_actions WHERE smoke_at > "
                                           "now() - interval '24 hours'")
-    aha = lessie_get(query="SELECT count(distinct(smoke_at)), count(distinct(users.id)) as id_count FROM users join "
+    aha = lassie_get(query="SELECT count(distinct(smoke_at)), count(distinct(users.id)) as id_count FROM users join "
                            "smoking_actions on users.id = smoking_actions.user_id WHERE users.created_at > now() - "
                            "interval '24 hours' HAVING count(distinct(smoke_at)) >= 5")
-    ret = lessie_get(query="SELECT count(sa.smoke_at),count(distinct(users.id)) FROM users join smoking_actions sa "
+    ret = lassie_get(query="SELECT count(sa.smoke_at),count(distinct(users.id)) FROM users join smoking_actions sa "
                            "on users.id = sa.user_id WHERE users.created_at between CURRENT_DATE - interval'3 days' "
                            "and current_date - interval'2 days' GROUP BY users.id HAVING count(sa.smoke_at) > 3")
-    landing_conversion_rate = timestamp.compare2_0(lessie_ga_metrics(startDate="1daysAgo", metrics="ga:sessions"),
+
+    landing_conversion_rate = timestamp.compare2_0(lessie_ga_metrics(startDate="1daysAgo", metrics="ga:newUsers"),
                                                    count_new_users_id.iloc[0]["count"])
     aha_rate = timestamp.compare2_0(aha.iloc[0]['id_count'], count_active_users.iloc[0]["count"])
+    # ваще не работает пока aha null
 
     client.chat_postMessage(channel=channel_id,
                             text="*Lassie Smoke*"
                                  f"\n Period: Day ({(timestamp.yesterday.strftime('%d ' + '%B'))}"
                                  f" - {datetime.today().strftime('%d ' + '%B')}) \n"
-                                 '\n Metric: Landing Sessions'
+                                 '\n Metric: Landing Users'
                                  '\n Description: Количество сессий по лендингу'
-                                 f'\n Value: {lessie_ga_metrics(startDate="1daysAgo", metrics="ga:sessions")} \n'
-                                 '\n Metric: Total Users'
-                                 '\n Description: Суммарное количество пользователей'
-                                 f'\n Value: {count_new_users_id.iloc[0]["count"]}\n' 
+                                 f'\n Value: {lessie_ga_metrics(startDate="1daysAgo", metrics="ga:users")} \n'
                                  '\n Metric: Landing Conversion Rate'
                                  '\n Description: Конверсия с лендинга в установку '
                                  f'\n Value: {landing_conversion_rate} \n'
                                  '\n Metric: Aha-moment rate'
                                  '\n Description: % пользователей нажавших "i smoked" как минимум 5 раз'
-                                 f'\n Value: {aha_rate} \n'
+                                 f'\n Value: тут будет aha_rate \n'
                                  '\n Metric: Active users'
                                  '\n Description: Количество активных пользователей'
                                  f'\n Value: {count_active_users.loc[0]["count"]} \n'
@@ -71,7 +70,15 @@ def lassie_month():
 
 @app.route('/lassie', methods=['POST'])
 def lassie():
-    pass
+    data = request.form
+    channel_id = data.get('channel_id')
+    total_users = lassie_get(query="select count(distinct(id)) from users")
+    client.chat_postMessage(channel=channel_id,
+                            text="*Lassie Smoke*"
+                                 f"\n Period: All time \n"
+                            '\n Metric: Total Users'
+                            '\n Description: Суммарное количество пользователей'
+                            f'\n Value: {total_users[0]["count"]}\n')
 
 
 def lessie_ga_metrics(startDate, metrics):
