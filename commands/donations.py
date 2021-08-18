4c6888bce3efb6591c75735de6500a5a6bb31a0d
AlexandrSligan <sligan.alex@gmail.com>
2021-08-19 00:08:34 +0700

    donation fix

--- commands/donations.py
+++ commands/donations.py
@@ -8,42 +8,42 @@ from datetime import datetime
 load_dotenv()
 
 
-@app.route('/business-day', methods=['POST'])
+@app.route('/Donations-day', methods=['POST'])
 def business_day():
     data = request.form
     channel_id = data.get('channel_id')
     client.chat_postMessage(channel=channel_id,
-                            text='*Business*'
+                            text='*Donations*'
                                  f"\n Period: Day ({(timestamp.yesterday.strftime('%d ' + '%B'))}"
                                  f" - {datetime.today().strftime('%d ' + '%B')}) \n"
                                  '\n Buy Me a Coffee donations:'
-                                 f'\n Value: {bmc(timestamps=timestamp.yesterday)} $')
+                                 f'\n Value: {bmc(timestamps=timestamp.yesterday)}$')
     return Response(), 200
 
 
-@app.route('/business-week', methods=['POST'])
+@app.route('/Donations-week', methods=['POST'])
 def business_week():
     data = request.form
     channel_id = data.get('channel_id')
     client.chat_postMessage(channel=channel_id,
-                            text='*Business*'
+                            text='*Donations*'
                                  f'\n Period: Week ({(timestamp.week.strftime("%d " + "%B"))}'
                                  f' - {datetime.today().strftime("%d " + "%B")}) \n'
                                  '\n Buy Me a Coffee donations:'
-                                 f'\n Value: {bmc(timestamps=timestamp.week)} $')
+                                 f'\n Value: {bmc(timestamps=timestamp.week)}$')
     return Response(), 200
 
 
-@app.route('/business-month', methods=['POST'])
+@app.route('/Donations-month', methods=['POST'])
 def business_month():
     data = request.form
     channel_id = data.get('channel_id')
     client.chat_postMessage(channel=channel_id,
-                            text='*Business*'
+                            text='*Donations*'
                                  f'\n Period: Month ({(timestamp.month.strftime("%d " + "%B"))}'
                                  f' - {datetime.today().strftime("%d " + "%B")}) \n'
                                  '\n Buy Me a Coffee donations:'
-                                 f'\n Value: {bmc(timestamps=timestamp.month)} $')
+                                 f'\n Value: {bmc(timestamps=timestamp.month)}$')
     return Response(), 200
 
 
